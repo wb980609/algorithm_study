83f27156c76e0ca45e8f0dad5761f86206c2ff7d
jwb6739 <jwb6739@gmail.com>
2024-10-17 16:30:08 +0900

    10818번 : 최소, 최대

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
-//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
-// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
-public class Main {
-    public static void main(String[] args) {
-        //TIP 캐럿을 강조 표시된 텍스트에 놓고 <shortcut actionId="ShowIntentionActions"/>을(를) 누르면
-        // IntelliJ IDEA이(가) 수정을 제안하는 것을 확인할 수 있습니다.
-        System.out.printf("Hello and welcome!");
+import java.util.Arrays;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP <shortcut actionId="Debug"/>을(를) 눌러 코드 디버그를 시작하세요. 1개의 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 중단점을 설정해 드렸습니다
-            // 언제든 <shortcut actionId="ToggleLineBreakpoint"/>을(를) 눌러 중단점을 더 추가할 수 있습니다.
-            System.out.println("i = " + i);
+public class Main {
+    public static void main (String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();           // 정수의 개수 N
+        int[] arr = new int[N];         // 둘째줄에 받아올 정수를 담을 크기가 N인 배열
+
+        for(int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
         }
+
+        Arrays.sort(arr);   // Arrays 내장함수 sort()를 사용하여 오름차순 정렬
+        System.out.println(arr[0] + " " + arr[N-1]);    // 오름차순 정렬이므로 첫번째 인덱스가 최소 마지막 인덱스가 최대
+
     }
 }
